50f58b67b60e3fc736df6fec7e4c2f16844b21c7
Carlos Avance <carlosavance@gmail.com>
2021-06-09 16:46:44 -0300

    add category: ok

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const connection = require("./database/database");
 
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
 const Article = require ("./articles/Article");
 const Category = require ("./categories/Category");
 
@@ -22,6 +25,9 @@ connection
     }).catch ((error) => {
         console.log(error);
     })
+
+app.use("/", categoriesController);
+app.use("/",articlesController)
 
 
 app.get("/", (req, res) => {
