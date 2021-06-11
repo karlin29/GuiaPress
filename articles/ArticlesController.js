406635f574109cea826542de737116521d8ce4cc
Carlos Avance <carlosavance@gmail.com>
2021-06-11 17:09:38 -0300

    Edit articles  in process

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,94 @@
 const express = require ("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req,res) => {
-    res.send("Route of articles")
+router.get("/admin/articles", (req,res) => {
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render("admin/articles/index", {articles: articles});
+    }); 
 });
 
 router.get("/admin/articles/new", (req,res) => {
-    res.render("admin/articles/new")
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories})
+    })
+    
+});
+
+router.post("/articles/save", (req,res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=> {
+        res.redirect("/admin/articles")
+    });
+
+});
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){ //  SE FOR DIFERENTE DE NULO / IF DIFFERENT OF NULL
+
+        if(!isNaN(id)){ // SE PASSAR EM TODAS AS VALIDAÇÕES / IF PASSED ALL VALIDATIONS
+
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(()=>{
+                res.redirect("/admin/articles");
+            });
+
+        } else { // SE NÃO FOR UM NÚMERO / IF NOT A NUMBER
+            res.redirect("/admin/articles");
+        }
+
+    } else { // SE FOR NULO / IF RESULT IS EQUALS A NULL
+
+        res.redirect("/admin/articles");
+
+    }
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Article.update({title: title, slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(()=> {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if (isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+
+            res.render("admin/articles/edit",{article: article});
+
+        }else{
+            res.redirect("/admin/articles"); 
+        }
+    });  
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require ("sequelize");
 
 const connection = new Sequelize('ucf132b2097b15cb7e042','ucf132b2097b15cb7e042','pe5224c6c36e5a7a61591',{
     host: 'midas.universidadeglobal.com.br',
-    dialect: "mysql"
+    dialect: "mysql",
+    timezone: "-03:00"
 });
 
 module.exports = connection;
